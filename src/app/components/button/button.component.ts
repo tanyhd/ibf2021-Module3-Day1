6a7bf82603315b7c46cbd1c0b0a35a2cb933c56c
Darren <darren@243hotmail.com>
2022-01-24 15:22:32 +0800

    Initial commit

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+  buttonText: string = "Click Me";
+  color: string = "green";
+
+  alertUser() {
+    alert("You should not click !!!");
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
